b994de9e2e1f99b232dbd82b2e3862f3a4635f92
giacomocapetta <152877696+giacomocapetta@users.noreply.github.com>
2024-07-03 01:07:59 +0000

    e anche oggi si dorme domani

--- script.js
+++ script.js
@@ -194,4 +194,32 @@ function closeSidebar() {
     
         window.addEventListener('resize', setPositionByIndex);
     });
-    
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+document.querySelectorAll('.img-progetti img').forEach(img => {
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal content, close the modal
+modal.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
